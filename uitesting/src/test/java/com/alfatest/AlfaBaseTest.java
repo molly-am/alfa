aecd473fc7df18c16b1b6536243194f40c2546e7
yahor <e.hudoleev@gmail.com>
2024-10-26 05:18:50 +0300

    modify: add afterMethod

--- uitesting/src/test/java/com/alfatest/AlfaBaseTest.java
+++ uitesting/src/test/java/com/alfatest/AlfaBaseTest.java
@@ -1,10 +1,9 @@
 package com.alfatest;
 
-
 import com.alfaframe.DriverManager;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
@@ -15,6 +14,12 @@ public class AlfaBaseTest {
     @BeforeSuite(alwaysRun = true, description = "Suite Level Setup")
     public void suiteSetUp() {
         LOGGER.info("Start suite");
+    }
+
+    @AfterMethod(alwaysRun = true, description = "Test tear down")
+    public void afterMethodSetup(){
+        LOGGER.info("Close opened driver after test");
+        DriverManager.closeDriver(DriverManager.getMobileDriver());
     }
 
     @AfterSuite(alwaysRun = true, description = "Suite Level Tear Down")
